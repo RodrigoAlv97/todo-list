1f8ca724fde25f3ad3d2a75b936b44382f7b319d
RodrigoA97 <rodrigo97wd@gmail.com>
2023-01-02 10:02:09 -0300

    delete project

--- src/modules/deleteProject.js
+++ src/modules/deleteProject.js
@@ -3,7 +3,6 @@ import { projectList } from "..";
 export const deleteProject = (index) => {
   if (index > -1) {
     projectList.splice(index, 1); 
-    console.log(projectList)
   }
 }
 
--- src/modules/loadProjectList.js
+++ src/modules/loadProjectList.js
@@ -1,3 +1,4 @@
+import { deleteProject } from "./deleteProject";
 import { showCurrentList } from "./showCurrentList";
 
 const projectList = document.querySelector('.side-bot');
@@ -7,11 +8,24 @@ const loadProjects = (arr) => {
   projectList.replaceChildren ()
   arr.forEach((element , index) => {
     const project = document.createElement('div');
-    project.textContent = element.name;
+    const projectName = document.createElement('div')
+    projectName.textContent = element.name;
+    projectName.setAttribute('class' , 'project-list-name')
     project.setAttribute('data' , `${index}`)
-    project.addEventListener('click' , () => {
+    project.setAttribute('class' , 'project-list-item')
+    projectName.addEventListener('click' , () => {
       showCurrentList(element.list , todoList)
     })
+    const btn = document.createElement('button')
+    btn.setAttribute('class' , 'project-list-btn')
+    btn.textContent = 'x'
+    btn.addEventListener('click' , () => {
+      deleteProject(index)
+      loadProjects(arr)
+    })
+    project.appendChild(projectName)
+    project.appendChild(btn)
+    
     projectList.appendChild(project)
   });
 }
